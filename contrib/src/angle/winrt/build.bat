dba476ba3f5446e541dc48a2ead6431648f19e48
Dale Stammen <dalestam@microsoft.com>
2016-05-03 13:21:26 -0700

    added SHA check

--- contrib/src/angle/winrt/build.bat
+++ contrib/src/angle/winrt/build.bat
@@ -1,9 +1,19 @@
 @echo off
+setlocal
 
 set VERSION=2.1.8
-set ANGLE_URL=http://api.nuget.org/packages/angle.windowsstore.%VERSION%.nupkg
+set URL=http://api.nuget.org/packages/angle.windowsstore.%VERSION%.nupkg
+set STARTDIR=%cd%
+set LOGFILE=%~dp0\build.log
 
+:: ---------------------------------------------------------------------------
+:: Clean previous build
+:: ---------------------------------------------------------------------------
 
+if exist %LOGFILE% (
+	rm -f %LOGFILE%
+)
+	
 if exist temp (
 	rm -rf temp
 )
@@ -15,16 +25,38 @@ if exist install (
 mkdir temp
 mkdir install
 
-pushd temp
-	echo downloading ANGLE version %VERSION%
-	if not exist angle.windowsstore.%VERSION%.nupkg (
-		curl -O -L %ANGLE_URL%
-	)
-	
-	unzip angle.windowsstore.%VERSION%.nupkg -d angle
+:: ---------------------------------------------------------------------------
+:: Download code if necessary
+:: ---------------------------------------------------------------------------
+
+pushd ..\..\..\tarballs
+	sha512sum --check ..\src\angle\winrt\SHA512SUMS
+
+	if %ERRORLEVEL% NEQ 0 (
+
+		if exist angle.windowsstore.%VERSION%.nupkg (
+			rm angle.windowsstore.%VERSION%.nupkg
+		)
+		
+		call :DO_LOG "Downloading angle.windowsstore.%VERSION%.nupkg..."
+		curl -O -L %URL%
+	)	
 popd
 
-echo Installing ANGLE...
+:: ---------------------------------------------------------------------------
+:: Decompress code
+:: ---------------------------------------------------------------------------
+
+pushd temp
+	call:DO_LOG "Decompressing angle.windowsstore.%VERSION%.nupkg..."
+	unzip ../../../../tarballs/angle.windowsstore.%VERSION%.nupkg -d angle
+popd
+
+:: ---------------------------------------------------------------------------
+:: Install Windows 10.0 and 8.1 libs in cocos2d-x v3 folder format
+:: ---------------------------------------------------------------------------
+
+call:DO_LOG "Installing ANGLE..."
 
 set INDIR=temp\angle\
 
@@ -47,8 +79,26 @@ set OUTDIR=install\wp_8.1-specific\angle\prebuilt
 xcopy "%INDIR%\bin\Phone\Win32" "%OUTDIR%\win32" /iycqs
 xcopy "%INDIR%\bin\Phone\ARM" "%OUTDIR%\arm" /iycqs
 
-echo ANGLE build complete.
+call:DO_LOG "ANGLE build complete."
 
+::--------------------------------------------------------
+::-- error_exit
+::		Note: Don't call anything that will change %ERRORLEVEL%
+::--------------------------------------------------------
+:error_exit
+endlocal
+exit \b %ERRORLEVEL%
+goto:eof
+
+::--------------------------------------------------------
+::-- DO_LOG
+::		%~1 message to log
+::--------------------------------------------------------
+:DO_LOG
+	echo %~1
+	echo %~1 >> %LOGFILE%
+	goto:eof
+	
 
 
 
